ac786dc54e1e5fbc54e30cac633ff29f0104affa
Benoit Calvez <benoit.calvez@gmail.com>
2012-05-12 11:56:26 +0200

    [missing] Add app "link_tracking", refs: #10

---
+++ collectr/link_tracking/views.py
@@ -0,0 +1,21 @@
+# Create your views here.
+from django.shortcuts import redirect
+
+from source.models import LinkSum, UrlViews
+
+def track_link(request, link_id):
+    link = LinkSum.objects.get(pk=link_id)
+    link.read = True
+    link.save()
+
+    try:
+        uv = UrlViews.objects.get(url__linksum__pk=link.pk)
+        uv.count += 1
+        uv.save()
+    except UrlViews.DoesNotExist:
+        url = link.url
+        uv = UrlViews.objects.create(count=1)
+        url.views = uv
+        url.save()
+
+    return redirect(link.link)
