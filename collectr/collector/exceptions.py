df7b8db33c87280e1b9f9f4713f53cd0c07d9f5c
Benoit Calvez <benoit.calvez@gmail.com>
2012-05-29 22:13:46 +0200

    Missing exceptions file

---
+++ collectr/collector/exceptions.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+    Base exception at collector.
+
+"""
+class BaseCollectorException(Exception):
+    pass
+
+class DeleteLinkException(BaseCollectorException):
+    pass
+
+class UnsupportedContentException(BaseCollectorException):
+    pass
+
+class UrlExtractException(BaseCollectorException):
+    pass
