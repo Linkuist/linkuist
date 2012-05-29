66d18e25e8d0937bc474e2f34077afe384e53f54
Benoit Calvez <benoit.calvez@gmail.com>
2012-05-29 23:26:54 +0200

    [twitter task] Fix link already in database

--- collectr/semantism/tasks.py
+++ collectr/semantism/tasks.py
@@ -312,12 +312,10 @@ class TwitterStatus(Task):
                 except Exception, exc:
                     url_m = Url.objects.get(link=url_parser.url)
 
-            try:
-                lsum = LinkSum.objects.filter(url__pk=url_m.pk, user__id=user_id).update(recommanded=F('recommanded') + 1)
+            links_numb = LinkSum.objects.filter(url__pk=url_m.pk, user__id=user_id).update(recommanded=F('recommanded') + 1)
+            if links_numb:
                 logger.info("url already in database")
                 continue
-            except LinkSum.DoesNotExist:
-                pass
 
             if url_parser.is_html_page():
                 url_parser.find_url_summary()
