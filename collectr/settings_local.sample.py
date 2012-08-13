068dc54327883580239c990be5b3ff482b52b68f
Gilles Fabio <gilles.fabio@gmail.com>
2012-08-13 20:50:35 +0200

    Added settings_local.sample.py.

---
+++ collectr/settings_local.sample.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Collectr local settings.
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'collectr',
+        'USER': 'collectr',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
