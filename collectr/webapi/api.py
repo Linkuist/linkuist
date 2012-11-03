41953f5831d1bd01c0dbb2e5cf4d7fb2ca739491
Benoit Calvez <benoit.calvez@gmail.com>
2012-11-03 18:16:29 +0100

    webapi: Adding sources resource

--- collectr/webapi/api.py
+++ collectr/webapi/api.py
@@ -1,7 +1,9 @@
 
-from tastypie import fields, utils
+from tastypie import fields
 from tastypie.resources import ModelResource
-from tastypie.authentication import MultiAuthentication, SessionAuthentication, ApiKeyAuthentication
+from tastypie.authentication import (MultiAuthentication,
+                                     SessionAuthentication,
+                                     ApiKeyAuthentication)
 from tastypie.authorization import Authorization
 
 from source import models as source_models
@@ -12,7 +14,8 @@ class UrlResource(ModelResource):
     class Meta:
         queryset = source_models.Url.objects.all()
         excludes = ['raw_tags', 'content']
-        authentication = MultiAuthentication(ApiKeyAuthentication(), SessionAuthentication())
+        authentication = MultiAuthentication(
+            ApiKeyAuthentication(), SessionAuthentication())
         authorization = (
             Authorization()
         )
@@ -21,18 +24,51 @@ class UrlResource(ModelResource):
 
     def apply_authorization_limits(self, request, object_list):
         if request and hasattr(request, 'user'):
-            return object_list.filter(linksum__user=request.user).order_by('-pk')
+            return object_list.filter(
+                linksum__user=request.user).order_by('-pk')
 
         return object_list.none()
+
+
+class TagResource(ModelResource):
+
+    class Meta:
+        queryset = source_models.Tag.objects.all()
+        authentication = MultiAuthentication(
+            ApiKeyAuthentication(), SessionAuthentication())
+        authorization = (
+            Authorization()
+        )
+
+        resource_name = 'tag'
+        ordering = ['pk']
+        exclude = ['slug']
+
+
+class AuthorResource(ModelResource):
+
+    class Meta:
+        queryset = source_models.Author.objects.all()
+        authentication = MultiAuthentication(
+            ApiKeyAuthentication(), SessionAuthentication())
+        authorization = (
+            Authorization()
+        )
+
+        resource_name = 'author'
+        ordering = ['-pk']
 
 
 class LinkSumResource(ModelResource):
 
     url = fields.ToOneField(UrlResource, 'url', full=True)
+    authors = fields.ToManyField(AuthorResource, 'authors', full=True)
+    tags = fields.ToManyField(TagResource, 'tags', full=True)
 
     class Meta:
         queryset = source_models.LinkSum.objects.all()
-        authentication = MultiAuthentication(ApiKeyAuthentication(), SessionAuthentication())
+        authentication = MultiAuthentication(
+            ApiKeyAuthentication(), SessionAuthentication())
         authorization = (
             Authorization()
         )
@@ -42,7 +78,81 @@ class LinkSumResource(ModelResource):
 
     def apply_authorization_limits(self, request, object_list):
         if request and hasattr(request, 'user'):
-            return object_list.select_related('url').filter(user=request.user).order_by('-pk')
-
+            return object_list.select_related('url').filter(
+                user=request.user).order_by('-pk')
         return object_list.none()
 
+
+class CollectionResource(ModelResource):
+
+    class Meta:
+        queryset = source_models.Collection.objects.all()
+        authentication = MultiAuthentication(
+            ApiKeyAuthentication(), SessionAuthentication())
+        authorization = (
+            Authorization()
+        )
+
+        resource_name = 'collection'
+        ordering = ['pk']
+        exclude = ['user']
+
+    def apply_authorization_limits(self, request, object_list):
+        if request and hasattr(request, 'user'):
+            return object_list.filter(user=request.user)
+        return object_list.none()
+
+
+class FilterResource(ModelResource):
+
+    class Meta:
+        queryset = source_models.Filter.objects.all()
+        authentication = MultiAuthentication(
+            ApiKeyAuthentication(), SessionAuthentication())
+        authorization = (
+            Authorization()
+        )
+
+        resource_name = 'filter'
+        ordering = ['pk']
+        exclude = ['user']
+
+    def apply_authorization_limits(self, request, object_list):
+        if request and hasattr(request, 'user'):
+            return object_list.filter(user=request.user)
+        return object_list.none()
+
+
+class SourceResource(ModelResource):
+
+    class Meta:
+        queryset = source_models.Source.objects.all()
+        authentication = MultiAuthentication(
+            ApiKeyAuthentication(), SessionAuthentication())
+        authorization = (
+            Authorization()
+        )
+
+        resource_name = 'source'
+        ordering = ['pk']
+        exclude = ['slug']
+
+
+class RssResource(ModelResource):
+
+    class Meta:
+        queryset = source_models.Rss.objects.all()
+        authentication = MultiAuthentication(
+            ApiKeyAuthentication(), SessionAuthentication())
+        authorization = (
+            Authorization()
+        )
+
+        resource_name = 'rss'
+        ordering = ['pk']
+        exclude = ['uers']
+
+    def apply_authorization_limits(self, request, object_list):
+        if request and hasattr(request, 'user'):
+            return object_list.filter(users=request.user)
+        return object_list.none()
--- collectr/webapi/urls.py
+++ collectr/webapi/urls.py
@@ -1,5 +1,5 @@
 # django
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import patterns, include
 
 from tastypie.api import Api
 
@@ -7,7 +7,9 @@ from webapi import api
 
 v1_api = Api(api_name='1.0')
 v1_api.register(api.LinkSumResource())
-v1_api.register(api.UrlResource())  
+v1_api.register(api.UrlResource())
+v1_api.register(api.RssResource())
+v1_api.register(api.CollectionResource())
 
 
 urlpatterns = patterns('',
