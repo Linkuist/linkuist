f0811e5bdc7081ae9fa3b682395d43b42b1bdef9
Benoit Calvez <benoit.calvez@gmail.com>
2013-03-23 14:04:54 +0100

    Update link_indexing django cmd

--- collectr/semantism/management/commands/link_indexing.py
+++ collectr/semantism/management/commands/link_indexing.py
@@ -3,12 +3,15 @@
 
 """
 
+# python
+import logging
 
 # django
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
 # rq
+import redis
 from rq import Queue, Connection, Worker
 
 # raven (sentry)
@@ -18,6 +21,8 @@ from rq.contrib.sentry import register_sentry
 # import django's models to preload code
 from source import models as source_models
 
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     args = u"<queue_name>"
@@ -32,7 +37,12 @@ class Command(BaseCommand):
             w = Worker(qs)
             sclient = Client(settings.SENTRY_DSN)
             register_sentry(sclient, w)
-            w.work()
+            try:
+                w.work()
+            except redis.exceptions.ConnectionError:
+                logger.info("Redis didn't respond, delaying reconnection")
+                time.sleep(10)
+
 
 
 
