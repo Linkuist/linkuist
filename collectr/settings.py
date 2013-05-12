d8280f1a0bd2a2b6a09465ba604acd34857cff70
Benoit Calvez <dzentoo@gmail.com>
2013-05-12 19:20:48 +0200

    settings: Update logging to remove deprecation warnigns

--- collectr/settings.py
+++ collectr/settings.py
@@ -117,16 +117,22 @@ LOGIN_REDIRECT_URL = '/collection/'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'handlers': {
+    'filters': {
+         'require_debug_false': {
+             '()': 'django.utils.log.RequireDebugFalse'
+         }
+     },
+     'handlers': {
         'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler',
+             'level': 'ERROR',
+             'filters': ['require_debug_false'],
+             'class': 'django.utils.log.AdminEmailHandler'
         },
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
         },
-    }, 
+    },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
