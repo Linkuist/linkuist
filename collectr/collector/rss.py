257c8bd00e98faeb7482a4857ddbf7c252c4afd5
Gilles Dartiguelongue <eva@gentoo.org>
2013-05-26 20:26:11 +0200

    collector: cosmetic fixes to rss check job

--- collectr/collector/rss.py
+++ collectr/collector/rss.py
@@ -1,66 +1,70 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
     Fetch rss and store them in the message broker
-
 """
 
-import feedparser
+# python
 import logging
 import os
 import sys
 import time
 import urlparse
-
 from datetime import datetime
+
+# 3rd party
+import feedparser
+import redis
+from rq import Queue, Connection
 
 sys.path.append('../')
 sys.path.append('../../')
 
 os.environ['DJANGO_SETTINGS_MODULE'] ='collectr.settings'
 
-from collectr import settings
-from django.core.management import setup_environ
-setup_environ(settings)
-
-# django
+from django.conf import settings
 from django.contrib.auth.models import User
 
 # semantism
 from source.models import Rss
 from semantism.process import index_url
 
-# rq
-import redis
-from rq import Queue, Connection
-
 
 def fetch_rss():
     logger = logging.getLogger(__name__)
     rss_feeds = Rss.objects.all()
+
     with Connection(redis.Redis(**settings.RQ_DATABASE)):
-        q = Queue('link_indexing')
+        queue = Queue('link_indexing')
 
         for rss_feed in rss_feeds:
             feed = feedparser.parse(rss_feed.link)
-
             urlp = urlparse.urlparse(rss_feed.link)
-            if 'etag' in feed and feed['etag'] != rss_feed.etag:
+
+            if feed.get('etag') != rss_feed.etag:
                 logger.info('New entry in feed %s', rss_feed.link)
+
                 for entry in feed.entries:
-                    date_pub = entry.get('published_parsed') or entry.get('updated_parsed')
+                    date_pub = entry.get('published_parsed') or \
+                        entry.get('updated_parsed')
                     if not date_pub:
                         logger.warning('Cannot find date_pub in feed %s',
                                        rss_feed.link)
                         continue
                     date_published = datetime(*date_pub[:-3])
 
-                    user_id_list = list(rss_feed.users.values_list('id', flat=True))
+                    user_id_list = list(rss_feed.users.values_list('id',
+                                                                   flat=True))
                     time.sleep(1)
 
-                    q.enqueue_call(func=index_url, args=(entry['link'], user_id_list,
-                            date_published, urlp.netloc, "Rss"), timeout=60)
+                    queue.enqueue_call(func=index_url, args=(
+                        entry['link'], user_id_list, date_published,
+                        urlp.netloc, "Rss"
+                    ), timeout=60)
                 rss_feed.etag = feed['etag']
                 rss_feed.save()
+
             else:
                 logger.info('Feed %s not updated', rss_feed.link)
 
