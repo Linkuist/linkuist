af67e72a4137e7052436255905e6410646281a47
Gilles Dartiguelongue <eva@gentoo.org>
2013-05-26 17:42:09 +0200

    collector: add a simple command to add Atom/RSS feeds

---
+++ collectr/collector/management/commands/import_rss.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+""" Adds RSS/Atom feeds to Linkuist. """
+
+# python
+
+# 3rd party
+import feedparser
+
+# django
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+# collectr
+from source.models import Rss
+
+
+class Command(BaseCommand):
+    args = '<url> <username>'
+    help = 'Adds an RSS/Atom feed'
+
+    def handle(self, *args, **kwargs):
+        feed_url, username = args
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise CommandError('User {0} does not exist.'.format(username))
+
+        feed = feedparser.parse(feed_url)
+
+        obj, _ = Rss.objects.get_or_create(link=feed['feed']['link'],
+                                           name=feed['feed']['title'],
+                                           etag=feed['etag'])
+        obj.users.add(user)
+        obj.save()
