028803b40786a707e712abb3d4a6f5efdb2237b5
Benoit Calvez <dzentoo@gmail.com>
2013-06-02 22:46:58 +0200

    collector: Fix rss collection, closes: #7

--- collectr/collector/rss.py
+++ collectr/collector/rss.py
@@ -21,10 +21,10 @@ from rq import Queue, Connection
 sys.path.append('../')
 sys.path.append('../../')
 
-os.environ['DJANGO_SETTINGS_MODULE'] ='collectr.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'collectr.settings'
+
 
 from django.conf import settings
-from django.contrib.auth.models import User
 
 # semantism
 from source.models import Rss
@@ -39,31 +39,46 @@ def fetch_rss():
         queue = Queue('link_indexing')
 
         for rss_feed in rss_feeds:
-            feed = feedparser.parse(rss_feed.link)
+            feed = feedparser.parse(rss_feed.link, etag=rss_feed.etag)
+            if feed.status > 399:
+                logger.warning('Got bad http status while fetching %s', rss_feed.link)
+                continue
+
+            if feed.status == 304:
+                logger.info('Feed %s not modified', rss_feed.link)
+                continue
+
+            if feed.bozo:
+                logger.warning('Problem while parsing feed %s (%s)', 
+                    rss_feed.link, feed.bozo_exception.getMessage)
+                continue
             urlp = urlparse.urlparse(feed['feed']['link'])
 
             if feed.get('etag') != rss_feed.etag:
                 logger.info('New entry in feed %s', rss_feed.link)
-
-                for entry in feed.entries:
-                    date_pub = entry.get('published_parsed') or \
-                        entry.get('updated_parsed')
-                    if not date_pub:
-                        logger.warning('Cannot find date_pub in feed %s',
-                                       rss_feed.link)
-                        continue
-                    date_published = datetime(*date_pub[:-3])
-
-                    user_id_list = list(rss_feed.users.values_list('id',
-                                                                   flat=True))
-                    time.sleep(1)
-
-                    queue.enqueue_call(func=index_url, args=(
-                        entry['link'], user_id_list, date_published,
-                        urlp.netloc, "Rss"
-                    ), timeout=60)
                 rss_feed.etag = feed['etag']
                 rss_feed.save()
+
+            else:
+                logger.info('Feed %s does not support etag', rss_feed.link)
+
+            for entry in feed.entries:
+                date_pub = entry.get('published_parsed') or \
+                    entry.get('updated_parsed')
+                if not date_pub:
+                    logger.warning('Cannot find date_pub in feed %s',
+                                   rss_feed.link)
+                    continue
+                date_published = datetime(*date_pub[:-3])
+
+                user_id_list = list(rss_feed.users.values_list('id',
+                                                               flat=True))
+                time.sleep(1)
+
+                queue.enqueue_call(func=index_url, args=(
+                    entry['link'], user_id_list, date_published,
+                    urlp.netloc, "Rss"
+                ), timeout=60)
 
             else:
                 logger.info('Feed %s not updated', rss_feed.link)
--- collectr/collector/tests.py
+++ collectr/collector/tests.py
@@ -1,16 +1,52 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+    Test the collectors (rss, twitter, reddit ...)
 """
 
+from django.conf import settings
 from django.test import TestCase
 
+import redis
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from source import models as source_models
+from source import factories as source_factories
+
+from . import rss as rss_module
+
+import feedparser
+
+
+class RssCollectorTestCase(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(RssCollectorTestCase, self).__init__(*args, **kwargs)
+        self.rss = None
+        self.redis_client = redis.Redis(**settings.RQ_DATABASE)
+
+    def tearDown(self):
+        self.redis_client.flushdb()
+
+    def make_initial_rss(self):
+        self.rss = source_factories.RssFactory()
+
+    def test_rss_collector(self):
+        atom_feed = source_factories.RssFactory(
+            link='https://github.com/dzen/collectr/commits/master.atom',
+            etag=None
+        )
+
+        rss_feed = source_factories.RssFactory(
+            link='http://www.moto-net.com/rss_actu.xml',
+        )
+        rss_module.fetch_rss()
+
+        # reload the rss
+        atom_feed = source_models.Rss.objects.get(pk=atom_feed.pk)
+
+        self.assertIsNotNone(atom_feed.etag)
+
+        expected_jobs_len = (
+            len(feedparser.parse(atom_feed.link)['entries']) +
+            len(feedparser.parse(rss_feed.link)['entries'])
+        )
+        job_len = len(self.redis_client.keys('rq:job:*'))
+        self.assertEqual(job_len, expected_jobs_len)
