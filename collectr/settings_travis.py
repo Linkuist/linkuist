ca0d3ca1910c5db8343b67e84eff7c8b56340faa
Benoit Calvez <dzentoo@gmail.com>
2013-06-23 12:34:05 +0200

    travis: Add travis configuration

---
+++ collectr/settings_travis.py
@@ -0,0 +1,13 @@
+from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'collectr',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres'
+    }
+}
+
+
+SKIP_SOUTH_TESTS = True
+SOUTH_TESTS_MIGRATE = True
